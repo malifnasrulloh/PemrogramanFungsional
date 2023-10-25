479a31850c5b5085fae17395fd6617d2c0c38162
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2023-10-25 10:08:27 +0700

    437 - Muhammad Alif Nasrulloh

--- modul3/kegiatan1&2.py
+++ modul3/kegiatan1&2.py
@@ -9,25 +9,24 @@ def konversi(w=0):
 
 
 data = ["3 minggu 3 hari 7 jam 21 menit",
-         "5 minggu 5 hari 8 jam 11 menit",
-         "7 minggu 1 hari 5 jam 33 menit"]
+        "5 minggu 5 hari 8 jam 11 menit",
+        "7 minggu 1 hari 5 jam 33 menit"]
 
 data = list(map(lambda x: list(filter(str.isnumeric, x.split())), data))
 
-print("data = ", data,end="\n\n")
+print("data = ", data, end="\n\n")
 
-
-#kegiatan 1 
+# kegiatan 1
 print("================ Kegiatan 1 ================")
 answer = []
 
 for i in data:
-    minggu=int(i[0])
-    hari=int(i[1])
+    minggu = int(i[0])
+    hari = int(i[1])
     jam = int(i[2])
     menit = int(i[3])
-    print("minggu = ",minggu, end="\t")
-    print("hari = ",hari, end="\t")
+    print("minggu = ", minggu, end="\t")
+    print("hari = ", hari, end="\t")
     print("jam = ", jam, end="\t")
     print("menit = ", menit, end="\t")
     konvert = konversi(minggu)(hari)(jam)(menit)
@@ -36,7 +35,7 @@ for i in data:
 
 print(f"\n\n{answer}")
 
-#kegiatan 2
+# kegiatan 2
 print("\n\n================ Kegiatan 2 ================")
 for i in data:
     print(i)
--- modul3/kegiatan3.py
+++ modul3/kegiatan3.py
@@ -3,9 +3,9 @@ random_list = [105, 3.1, "Hello World", 737,
 
 data_float = tuple(filter(lambda x: type(x) == float, random_list))
 data_string = list(filter(lambda x: type(x) == str, random_list))
-data_int = list(map(lambda x:{"ratusan":str(x)[0],"puluhan":str(x)[1],"satuan":str(x)[2]},list(filter(lambda x: type(x) == int, random_list))))
-print("data_float : " , data_float)
-print("data_string : " , data_string)
-print("data_int : " , end="\n")
+data_int = list(map(lambda x: {"ratusan": str(x)[0], "puluhan": str(x)[1], "satuan": str(x)[2]}, list(filter(lambda x: type(x) == int, random_list))))
+print("data_float : ", data_float)
+print("data_string : ", data_string)
+print("data_int : ", end="\n")
 for i in data_int:
     print(i)
